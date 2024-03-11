e6d1c3f63bd933d87b33ab9b0df4694c0c99efaa
ondghoskot <anaschtioui65@gmail.com>
2024-03-11 05:28:37 +0000

    Implemented JSON serialization and deserialization

--- models/base_model.py
+++ models/base_model.py
@@ -22,6 +22,8 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """prints string represenatation of instance"""
@@ -32,6 +34,7 @@ class BaseModel:
         """updates the public instance attribute updated_at
            with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of
---
+++ models/engine/file_storage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""File storage engine"""
+import json
+import pathlib
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """ serializes instances to a JSON file and deserializes JSON
+    file to instances"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        k = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[k] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        dicti = {}
+        for k, v in FileStorage.__objects.items():
+            dicti[k] = v.to_dict()
+        with open(FileStorage.__file_path, mode="w") as file0:
+            json.dump(dicti, file0)
+
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        file_path = Path(FileStorage.__file_path)
+        if file_path.isfile():
+            with open(FileStorage.__file_path, mode="r") as file1:
+                FileStorage.__objects = {}
+                for k, v in json.load(file1).items():
+                    FileStorage.__file_path[k] = BaseModel(**v)
