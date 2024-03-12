45d5029e6af59cc8f171d546ca8ec4ae382c26f0
ondghoskot <anaschtioui65@gmail.com>
2024-03-12 14:12:05 +0000

    implemented create and show commands

--- console.py
+++ console.py
@@ -23,6 +23,35 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         pass
 
+    def do_create(self, arg0):
+        """Creates a new instance of BaseModel, saves it
+        (to the JSON file) and prints the id"""
+        if arg0 == "":
+            print("** class name missing **")
+        elif arg0 == "BaseModel":
+            instance = models.base_model.BaseModel()
+            instance.save()
+            print(instance.id)
+        else:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg0):
+        """Prints the string representation of
+        an instance based on the class name and id"""
+        arguments = arg0.split()
+        if len(arguments) == 0:
+            print ("** class name missing **")
+        elif arguments[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(arguments) != 2:
+            print("** instance id missing **")
+        else:
+            line = arguments[0] + "." + arguments[1]
+            if line in models.storage.all():
+                print(models.storage.all()[line])
+            else:
+                print("** no instance found **")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
