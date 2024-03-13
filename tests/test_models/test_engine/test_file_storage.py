e4d5e5644c7e9b0cd5b560cc2c119b6bbae6e2ce
ondghoskot <anaschtioui65@gmail.com>
2024-03-13 15:06:01 +0000

    started unittest for file_storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,4 +6,6 @@ import unittest
 
 class TestFileStorage(unittest.TestCase):
     """Tests for the storage engine class"""
-
+    def test_file_path(self):
+        """tests if __file_path is a str"""
+        self.assertTrue(type(FileStorage._FileStorage__file_path) == str)
