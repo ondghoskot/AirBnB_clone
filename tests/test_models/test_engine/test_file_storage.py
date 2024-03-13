78e8b7d6f3f7dccc4cb5dbe21eba52531d60b90a
ondghoskot <anaschtioui65@gmail.com>
2024-03-13 14:38:55 +0000

    cleanup

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Test file for FileStorage"""
+from models.engine.file_storage import FileStorage
+import unittest
+
+
+class TestFileStorage(unittest.TestCase):
+    """Tests for the storage engine class"""
+
